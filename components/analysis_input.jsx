caf96d1f82f25b1cff40b3e9bec3972c5ec2fd70
Ansar331 <tiplar85@gmail.com>
2023-07-20 22:58:35 +0600

    update analysis_input

--- components/analysis_input.jsx
+++ components/analysis_input.jsx
@@ -1,5 +1,3 @@
-// analysis_input.jsx
-
 import { useState } from 'react';
 import { analyzeResume } from '/pages/api';
 import { useSession } from 'next-auth/react';
@@ -41,10 +39,10 @@ const Analysis = () => {
   };
 
   return (
-    <div className="flex flex-col justify-center items-center min-h-screen">
-      <form onSubmit={handleSubmit} className="w-4/5 md:w-1/3 bg-white shadow-md rounded px-8 py-6" style={{ marginTop: '-300px' }}>
+    <div className="relative min-h-screen overflow-hidden">
+      {/* Form */}
+      <form onSubmit={handleSubmit} className="mt-8 w-4/5 md:w-1/3 mx-auto bg-white shadow-md rounded px-8 py-6">
         <h2 className="text-2xl font-bold mb-6">Сделать анализ резюме!</h2>
-
         <div className="mb-4">
           <label htmlFor="file" className="block text-gray-700 text-sm font-bold mb-2">
             Загрузите ваше резюме:
@@ -52,12 +50,10 @@ const Analysis = () => {
           <input
             type="file"
             id="file"
-            accept=".pdf"
             onChange={handleFileChange}
             required
           />
         </div>
-
         <div className="flex items-center justify-center md:justify-between">
           <button
             type="submit"
@@ -67,13 +63,20 @@ const Analysis = () => {
           </button>
         </div>
       </form>
+      {/* End of form */}
 
+      {/* Result text */}
       {output && (
-        <div className="mt-8 w-4/5 md:w-1/3">
+        <div className="mt-8 w-4/5 md:w-1/3 mx-auto">
           <h2 className="text-2xl font-bold mb-2">Результат</h2>
           <p>{output}</p>
         </div>
       )}
+      <style jsx>{`
+        .min-h-screen {
+          min-height: 50vh; /* Adjust the value as needed */
+        }
+      `}</style>
     </div>
   );
 };
