96b5e063a92e477af00da433631ce36c60fe35dd
Ansar331 <tiplar85@gmail.com>
2023-07-25 10:14:27 +0600

    update header and improve

--- components/improve_input.jsx
+++ components/improve_input.jsx
@@ -4,9 +4,8 @@ import { useSession } from 'next-auth/react';
 
 const MyForm = () => {
   const [file, setFile] = useState('');
-  const [data, setData] = useState('');
   const [output, setOutput] = useState('');
-  const [isLoading, setIsLoading] = useState(false); // Добавлено состояние загрузки
+  const [isLoading, setIsLoading] = useState(false);
   const { data: session } = useSession();
   const user_id = (session && session.user.email) ? session.user.email : "";
 
@@ -14,18 +13,14 @@ const MyForm = () => {
     setFile(e.target.value);
   };
 
-  const handleDataChange = (e) => {
-    setData(e.target.value);
-  };
-
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
-      setIsLoading(true); // Устанавливаем состояние загрузки в true
+      setIsLoading(true);
 
       // Send data to API for processing
-      const resumeData = { file, data, user_id };
+      const resumeData = { file, user_id };
       const response = await improveResume(resumeData);
 
       // Set the processed output
@@ -34,13 +29,16 @@ const MyForm = () => {
       // Handle error
       console.error(error);
     } finally {
-      setIsLoading(false); // Устанавливаем состояние загрузки обратно в false
+      setIsLoading(false);
     }
   };
 
   return (
-    <div className="flex flex-col justify-center items-center min-h-screen">
-      <form onSubmit={handleSubmit} className="w-4/5 md:w-1/3 bg-white shadow-md rounded px-8 py-6" style={{ marginTop: '-300px' }}>
+    <div className="min-h-screen flex flex-col justify-center items-center">
+      <form
+        onSubmit={handleSubmit}
+        className="w-4/5 md:w-1/3 bg-white shadow-md rounded px-8 py-6"
+      >
         <h2 className="text-2xl font-bold mb-6">Улучшить резюме!</h2>
 
         <div className="mb-4">
@@ -57,26 +55,12 @@ const MyForm = () => {
           />
         </div>
 
-        <div className="mb-4">
-          <label htmlFor="data" className="block text-gray-700 text-sm font-bold mb-2">
-            Напишите должность, которую вы хотите:
-          </label>
-          <textarea
-            id="data"
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            placeholder="Введите данные"
-            value={data}
-            onChange={handleDataChange}
-            required
-          />
-        </div>
-
         <div className="flex items-center justify-center md:justify-between">
           <button
             type="submit"
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full md:w-auto"
           >
-            {isLoading ? 'Печатает...' : 'Отправить'} {/* Изменено на "Печатает..." во время загрузки */}
+            {isLoading ? 'Печатает...' : 'Отправить'}
           </button>
         </div>
       </form>
@@ -87,6 +71,14 @@ const MyForm = () => {
           <p>{output}</p>
         </div>
       )}
+
+      {/* Add an empty div to extend the page height */}
+      <div className="flex-grow"></div>
+      <style jsx>{`
+        .min-h-screen {
+          min-height: 50vh; /* Adjust the value as needed */
+        }
+      `}</style>
     </div>
   );
 };
